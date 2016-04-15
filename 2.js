25eb574718884ba4a95a58cbf0e924d06ae6a2b5
parakawa <paty.arakawa@gmail.com>
2016-04-15 17:32:46 -0500

    problema 8, continuara

---
+++ 2.js
@@ -0,0 +1,8 @@
+function mayor(a,b){
+	if (a>=b)
+		console.log("el mayor es " +a);
+	else 
+		console.log("el mayor es " +b);
+}
+
+mayor(-3,3);
---
+++ 8.js
@@ -0,0 +1,12 @@
+function costo(kilometros,bus,personas){
+	switch(expression) {
+    case n:
+        code block
+        break;
+    case n:
+        code block
+        break;
+    default:
+        default code block
+	}
+}
