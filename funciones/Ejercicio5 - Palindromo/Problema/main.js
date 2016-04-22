e8b684bfa7f488232d141c582bce8e0d2589f8ac
parakawa <paty.arakawa@gmail.com>
2016-04-22 12:20:00 -0500

    fin funciones, me falta palindromo

--- funciones/Ejercicio5 - Palindromo/Problema/main.js
+++ funciones/Ejercicio5 - Palindromo/Problema/main.js
@@ -1,7 +1,29 @@
 function palindromo(){
-	var x=prompt("Ingresa una frase que creas que es palindrómana");
-	var 
+	var palabra=prompt("Ingresa una frase que creas que es palindrómana");
+	var palabra=palabra.toLowerCase().split("");
+	var tamanio=palabra.length;
+		for(var i=0;i<tamanio;i++)
+			if(palabra[i]==" ")
+				palabra.splice(i,1);
+			var n=palabra.length;
+			var aux=null;
+		for(var j=0;j<n;j++)
+			if(palabra[j]==palabra[n-j])
+				aux.push(1);
+			else 
+				aux.push(0);
+		while()
 
 }
-
   
+palindromo();
+/*for(var j=0;j<palabra.length;j++)
+		if()
+	for(var i=0;i<tamanio;i++)
+		if(palabra[i]==" ")
+			delete palabra[i];
+			document.write(palabra[i]);
+		
+*/
+
+
--- funciones/Ejercicio8 - Numero primo/Problema/main.js
+++ funciones/Ejercicio8 - Numero primo/Problema/main.js
@@ -1,4 +1,15 @@
-// Escribe aquí tu codigo Javascript
+function primo(){
+	var n=parseInt(prompt("Ingrese el número a testear:"));
+	var i=2;
+	while(i<n){
+		if(n%i==0){
+			document.write("El número "+n+" no es primo");
+			break;}
+		else 
+			i++;
+	}
+	if(i==n)
+		document.write("El número "+n+" es primo");
+}
 
-
-
+primo();
