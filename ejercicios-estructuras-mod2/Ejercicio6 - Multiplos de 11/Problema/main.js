eea5678a7fd3411a1ee314c62d268fc60f18a252
parakawa <paty.arakawa@gmail.com>
2016-05-04 17:29:30 -0500

    problema multiplo de 11

--- ejercicios-estructuras-mod2/Ejercicio6 - Multiplos de 11/Problema/main.js
+++ ejercicios-estructuras-mod2/Ejercicio6 - Multiplos de 11/Problema/main.js
@@ -1,4 +1,15 @@
-// Escribe aquí tu codigo Javascript
+var k=1;
+var n=0;
+var suma=0;
+while(n<3000){
+	n=11*k;
+	if(n<3000){
+		document.write(n+'<br>');
+		suma=suma+n;
+	}
+		
+	k++;
 
+} 
 
-
+document.write("la suma de todos los múltiplos de 11 menores a 3000 es "+suma);
--- funciones/Ejercicio3 - Ordenando numeros/Problema/main.js
+++ funciones/Ejercicio3 - Ordenando numeros/Problema/main.js
@@ -1,4 +1,19 @@
-// Escribe aquí tu codigo Javascript
+function ordenado(){
+	var x;
+	for(var i=0;i<3;i++)
+		x[i]=parseInt(prompt("Ingrese el valor "+(i+1)+":"));
+	for(var k=0;k<3;k++){ 
+		may=x[0];
+		men=x[0];
+		med=x[0];
+		if(may<=x[i])
+			may=x[i];
+		if(men>=x[i])
+			men=x[i];
+		if(may!=x[i] && men!=x[i])
+			med=x[i];
+	}
+	document.write("el orden ascendente es: "+men+" "+med+" "+may);
+}
 
-
-
+ordenado();
